81cc40efd3f0b647acf094dd85f752576b47e287
porfiriopaiz@gmail.com <porfiriopaiz@gmail.com>
2017-03-26 21:20:23 -0600

    Fixing minor mistakes

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Porfirio Páiz'
 TAGLINE = '(■_■¬)'
-SITEURL = 'https://localhost:8000'
-USER_LOGO_URL = 'http://localhost:8000/images/prpd8Vi.png'
+SITEURL = 'https://porfiriopaiz.github.io/site'
+USER_LOGO_URL = 'https://porfiriopaiz.github.io/site/images/prpd8Vi.png'
 SITENAME = "porfirio's blog"
 DISQUS_SITENAME = 'porfiriopaizsite'
 
